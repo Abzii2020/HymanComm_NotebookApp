eb8156e99249e436323baad382a0ff0bb88c377b
Dwayne Richards <DRICH2006@hotmail.com>
2020-04-21 20:41:28 -0500

    Adjustments to the Homepage

--- NB_Homepage.Designer.cs
+++ NB_Homepage.Designer.cs
@@ -37,19 +37,21 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(243, 124);
+            this.label1.Location = new System.Drawing.Point(707, 210);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(756, 73);
+            this.label1.Size = new System.Drawing.Size(1508, 145);
             this.label1.TabIndex = 0;
             this.label1.Text = "Notebook Application";
             // 
             // Login_btn
             // 
-            this.Login_btn.BackColor = System.Drawing.Color.FloralWhite;
+            this.Login_btn.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Login_btn.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login_btn.Location = new System.Drawing.Point(571, 216);
+            this.Login_btn.Location = new System.Drawing.Point(1347, 414);
+            this.Login_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Login_btn.Name = "Login_btn";
-            this.Login_btn.Size = new System.Drawing.Size(428, 57);
+            this.Login_btn.Size = new System.Drawing.Size(856, 110);
             this.Login_btn.TabIndex = 1;
             this.Login_btn.Text = "Click here to Login";
             this.Login_btn.UseVisualStyleBackColor = false;
@@ -57,13 +59,14 @@
             // 
             // NB_Homepage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::HymanComm_NotebookApp.Properties.Resources.formImg3;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1109, 403);
+            this.ClientSize = new System.Drawing.Size(2218, 781);
             this.Controls.Add(this.Login_btn);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "NB_Homepage";
             this.Text = "Welcome to Notebook ";
             this.ResumeLayout(false);
